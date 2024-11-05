88fe8737043606398aa22b3fd6ba21044868933a
Trương Anh Hiếu - KV Booking <hieu.ta@kiotviet.com>
2024-11-05 17:25:46 +0700

    init-prj"

---
+++ RabbitMqServices/MessageBroker/IMessagePublisher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqServices.MessageBroker;
+internal class IMessagePublisher
+{
+}
